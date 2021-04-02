42ed1662cb4cbe84d1de92a668e41990e2f74774
Elijah Collins <ecollins152@west-mec.org>
2021-04-02 15:18:05 -0700

    welp, restarting

--- js/main.js
+++ js/main.js
@@ -1,36 +1,19 @@
-$(function() {
+(function($) {
 
-  'use strict';
+	"use strict";
 
-  $('.js-menu-toggle').click(function(e) {
+	var fullHeight = function() {
 
-  	var $this = $(this);
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
 
-  	
+	};
+	fullHeight();
 
-  	if ( $('body').hasClass('show-sidebar') ) {
-  		$('body').removeClass('show-sidebar');
-  		$this.removeClass('active');
-  	} else {
-  		$('body').addClass('show-sidebar');	
-  		$this.addClass('active');
-  	}
-
-  	e.preventDefault();
-
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
   });
 
-  // click outisde offcanvas
-	$(document).mouseup(function(e) {
-    var container = $(".sidebar");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-      if ( $('body').hasClass('show-sidebar') ) {
-				$('body').removeClass('show-sidebar');
-				$('body').find('.js-menu-toggle').removeClass('active');
-			}
-    }
-	}); 
-
-    
-
-});
+})(jQuery);
