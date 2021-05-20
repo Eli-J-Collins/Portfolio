2407919e61508840018d15ab8bc5dd040121583b
Elijah Collins <ecollins152@west-mec.org>
2021-05-20 15:18:12 -0700

    more work

--- js/main.js
+++ js/main.js
@@ -17,3 +17,28 @@
   });
 
 })(jQuery);
+
+// Testimonials carousel (uses the Owl Carousel library)
+$(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
